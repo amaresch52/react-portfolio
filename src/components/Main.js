4605b622bb1dbbd1def52aacc4397094511da4a5
Tom Hanks <amaresch@gmail.com>
2021-08-06 00:35:15 -0500

    added projects and main code

---
+++ src/components/Main.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Nav from "./Nav";
+import About from "./About";
+import Contact from "./Contact";
+import Projects from "./Projects";
+
+export default function Main() {
+  const [currentPage, setCurrentPage] = useState("About");
+
+  const renderPage = (page) => {
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Projects") {
+      return <Projects />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <main>
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </main>
+  );
+}
